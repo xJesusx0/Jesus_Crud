88f19d7d2c0de06544b13ef77969f7b4fbf692f2
xJesusx0 <jesuspl0411@gmail.com>
2023-07-27 12:53:48 -0500

    deleted useless files,added delete function

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ import flask as flask
 import my_functions as sw
 app = flask.Flask(__name__)
 
-@app.route("/")
-def index():
 
-    data = sw.process_data()
-    return flask.render_template("index.html",data = data)
-
-@app.route("/registrar", methods = ["GET","POST"])
+@app.route("/", methods = ["GET","POST"])
 def register():
     data = sw.process_data()
     if flask.request.method  == "POST":
-
         usuario = flask.request.form["username"]
         contraseña = flask.request.form["password"]
 
@@ -20,11 +14,9 @@ def register():
     
         if user_request == True:
             error = "ese usuario ya esta ocupado"
-
             return flask.render_template("index3.html",error = error,data=data)
 
         elif user_request == False: 
-
             if usuario != "" and contraseña != "" :
                 sw.registrar(usuario, contraseña)
             
@@ -33,26 +25,19 @@ def register():
     data = sw.process_data()
     return flask.render_template("index3.html",data = data)
 
-
 @app.route("/tabla", methods = ["GET","POST"])
 def tabla():
     data = sw.process_data()
     return flask.render_template("tabla.html",
                                  data = data)
 
-@app.route("/eliminar", methods=["GET","POST"])
-def eliminar():
-    data = sw.process_data()
-    
-    if flask.request.method == "POST":
-        try:
-            id = flask.request.form["id"]
-            converted_id = int(id)
-            sw.new_delete(converted_id)
-        except ValueError:
-            return flask.render_template("delete.html",data=data)
+@app.route("/eliminar/<int:id>")
+def prueba_eliminar(id):
+    try:
+        sw.new_delete(id)
+    except ValueError:
+        pass
+    return flask.redirect("/")
 
-    data = sw.process_data()
-    return flask.render_template("delete.html",data=data)
 
 app.run(debug = True,port=4000) 
